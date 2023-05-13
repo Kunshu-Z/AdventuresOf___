0a915e3d32121abf41cb84c8febab5d4f081097f
Parth Talwar <parthtalwar@hotmail.com>
2023-05-13 21:06:38 +1200

    Created two forms: Title and Gameplay, and programmed Title buttons to quit the game and start the game respectively

---
+++ Gameplay.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace AdventuresOf___
+{
+    partial class Gameplay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            gameTxtLbl = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // gameTxtLbl
+            // 
+            gameTxtLbl.BorderStyle = BorderStyle.Fixed3D;
+            gameTxtLbl.Font = new Font("Verdana", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            gameTxtLbl.ForeColor = SystemColors.ButtonHighlight;
+            gameTxtLbl.Location = new Point(11, 13);
+            gameTxtLbl.Name = "gameTxtLbl";
+            gameTxtLbl.Size = new Size(777, 266);
+            gameTxtLbl.TabIndex = 0;
+            gameTxtLbl.Text = "Game Text";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Verdana", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(101, 338);
+            button1.Name = "button1";
+            button1.Size = new Size(149, 52);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Verdana", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(567, 338);
+            button2.Name = "button2";
+            button2.Size = new Size(149, 52);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Gameplay
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(gameTxtLbl);
+            Name = "Gameplay";
+            Text = "Gameplay";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label gameTxtLbl;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ Title.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace AdventuresOf___
+{
+    partial class Title
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            titleLabel = new Label();
+            startButton = new Button();
+            quitButton = new Button();
+            SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            titleLabel.AutoSize = true;
+            titleLabel.Font = new Font("Verdana", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            titleLabel.ForeColor = SystemColors.ButtonHighlight;
+            titleLabel.Location = new Point(225, 75);
+            titleLabel.Name = "titleLabel";
+            titleLabel.Size = new Size(350, 38);
+            titleLabel.TabIndex = 1;
+            titleLabel.Text = "Adventures of ___";
+            titleLabel.Click += titleLabel_Click;
+            // 
+            // startButton
+            // 
+            startButton.Font = new Font("Verdana", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            startButton.Location = new Point(335, 169);
+            startButton.Name = "startButton";
+            startButton.Size = new Size(128, 46);
+            startButton.TabIndex = 2;
+            startButton.Text = "Start";
+            startButton.UseVisualStyleBackColor = true;
+            startButton.Click += startButton_Click;
+            // 
+            // quitButton
+            // 
+            quitButton.Font = new Font("Verdana", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            quitButton.Location = new Point(335, 264);
+            quitButton.Name = "quitButton";
+            quitButton.Size = new Size(128, 46);
+            quitButton.TabIndex = 3;
+            quitButton.Text = "Quit";
+            quitButton.UseVisualStyleBackColor = true;
+            quitButton.Click += quitButton_Click;
+            // 
+            // Title
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(800, 450);
+            Controls.Add(quitButton);
+            Controls.Add(startButton);
+            Controls.Add(titleLabel);
+            Name = "Title";
+            Text = "Title";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label titleLabel;
+        private Button startButton;
+        private Button quitButton;
+    }
+}
